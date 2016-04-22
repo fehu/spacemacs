2481cc3d6ed584927abf33e89a559697ae299979
fehu <lord.fehu@gmail.com>
2016-04-21 21:44:19 -0500

    spellcheck splits words (S-<f8>)

---
+++ switch-spell-together.el
@@ -0,0 +1,31 @@
+(make-variable-buffer-local 'spell-together)
+(make-variable-buffer-local 'spell-together-min)
+(make-variable-buffer-local 'spell-together-lim)
+
+(setq-default spell-together nil)
+(setq-default spell-together-min 2)
+(setq-default spell-together-lim 6)
+
+
+
+(defun switch-spell-together()
+  (interactive)
+  (setq spell-together (not spell-together))
+  (let* ((margs '("--sug-mode=ultra"))
+         (targs (list "--run-together"
+                      (concat "--run-together-min=" (number-to-string
+                                                     (symbol-value 'spell-together-min)))
+                      (concat "--run-together-limit=" (number-to-string
+                                                       (symbol-value 'spell-together-lim)))
+                      ))
+         (args (if spell-together (append margs targs) margs))
+        )
+    (setq ispell-extra-args args)
+    (ispell-set-spellchecker-params)
+    (message (if spell-together "Set 'together' spelling" "Set whole spelling"))
+    )
+  )
+
+(global-set-key (kbd "S-<f8>") 'switch-spell-together)
+
+(provide 'switch-spell-together)
